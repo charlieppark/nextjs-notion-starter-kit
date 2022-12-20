cbc71192cb2b7d98af56cb063ce14588928dc862
charlieppark <chanho0309@gmail.com>
2022-12-21 04:02:58 +0900

    feat: initialize with custom notion

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '8b5129fb4a5343a0a5a70a2d9c52342b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Charlieppark Devlog',
+  domain: 'charlieppark.devlog.it',
+  author: 'Charlie Park',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Charlie Park Developer Log',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
